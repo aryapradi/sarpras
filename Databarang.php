879fee7fb9eaa8ccad40e4653df2ac3f590fa741
aryapradi <aryajanuary06@gmail.com>
2022-09-19 17:40:53 +0700

    nambah isi php belum jadi

---
+++ Databarang.php
@@ -0,0 +1,25 @@
+<?php
+    include 'koneksi.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="barang.css">
+    <title>Document</title>
+</head>
+<body>
+<body>
+    <div class="sidebar">
+        <header>SarPras</header>
+        <ul>
+            <li><a href="Databarang.php"><i class="#"></i>Data Barang</a></li>
+            <li><a href="Datapeminjam.php"><i class="#"></i>Data Peminjam</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
